a0bd99c0ad541e5d8570d848f7f7b940fc47527e
ChaudharySamirZafar <88251610+ChaudharySamirZafar@users.noreply.github.com>
2023-02-20 13:50:55 +0000

    - Rectified Silly Mistake

--- src/main/java/codenemy/api/Util/CompilerUtility.java
+++ src/main/java/codenemy/api/Util/CompilerUtility.java
@@ -47,15 +47,15 @@ public class CompilerUtility {
             return null;
         }
 
-        if (!pistonResponse.run().stderr().isEmpty() || !pistonResponse.run().stderr().isBlank()
-                || pistonResponse.run().signal() != null) {
+        if (pistonResponse.run().signal() != null && pistonResponse.run().signal().equalsIgnoreCase("SIGKILL")) {
+            testCaseResult.setError(List.of("Your code exceeded the runtime time limit\n"));
+            return testCaseResult;
+        }
+
+        if (!pistonResponse.run().stderr().isEmpty() || !pistonResponse.run().stderr().isBlank()) {
             // Get the errors.
-            if (pistonResponse.run().signal().equalsIgnoreCase("SIGKILL")) {
-                testCaseResult.setError(List.of("Your code exceeded the runtime time limit\n"));
-            } else {
-                List<String> errors = Arrays.asList(pistonResponse.run().stderr().split("\n"));
-                testCaseResult.setError(errors);
-            }
+            List<String> errors = Arrays.asList(pistonResponse.run().stderr().split("\n"));
+            testCaseResult.setError(errors);
             return testCaseResult;
         }
 
